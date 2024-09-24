1e0d19181850229d7fbedf00ec0da5863002677f
otvertka <otvertka123@ukr.net>
2024-09-24 15:17:11 +0200

    Re-add client folder content

---
+++ client/src/App.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+// import "./layout.scss"
+// import Navbar from "./components/navbar/Navbar";
+import HomePage from "./pages/homePage/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./pages/listPage/listPage";
+import Layout from "./pages/layout/layout";
+import SinglePage from "./pages/singlePage/singlePage";
+import 'leaflet/dist/leaflet.css'
+import ProfilePage from "./pages/profilePage/profilePage";
+import Register from "./pages/registerPage/register";
+import Login from "./pages/loginPage/login";
+
+const App: React.FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/register",
+          element: <Register />
+        }
+      ]
+    }
+  ]);
+
+  return (
+
+
+
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
